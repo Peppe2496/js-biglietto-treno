d08caca64bdc4a2f62af97895be5ba3346dc92ac
Peppe2496 <giuseppemusellajob@gmail.com>
2023-11-13 14:23:44 +0100

    created repo

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Questo è il file che contiene tutto il codice JS della pagina web
+
+// Questo è il file che contiene tutto il codice JS della pagina web
+
+// 1. Chiedere numero di km ed età del passeggero
+// 2. Calcorare il prezzo totale del viaggio 
+// 3. Il prezzo varia in base ai km (0.21 euro al km)
+// 4. Sconto del 20% per minorenni.
+// 5. Sconto del 40% per over 65
+
+
+let kilometersTodo = ("Inserisci il numero di chilometri da percorrere");
+let Age = ("Inserisci la tua età");
+
+
+
